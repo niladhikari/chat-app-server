c1874da84a0475003dc29de35597e5c93dc5552e
niladhikari <niladhikari445@gmail.com>
2024-02-18 19:05:24 +0600

    add the server side for the chat app....there i add many package for the chat server...there i create the signin,login ,logout jwt server using mongoos...there user profile randomly beacuse i use the json placeholder

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+dotenv.config();
+
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Start Server
+app.listen(PORT, () => {
+    connectToMongoDB();
+  console.log(`Server is running on port ${PORT}`);
+});
